032cdabd846048e32cf1f22ec8f272bbf86444ca
efbar <17676301+efbar@users.noreply.github.com>
2020-10-07 14:49:00 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,50 @@
+module "vault_cluster" {
+  source                   = "git::ssh://git@github.com/bitrockteam/hashicorp-vault-baseline//modules/cluster-raft?ref=master"
+  unseal_project_id        = var.unseal_project_id
+  unseal_keyring           = var.unseal_keyring
+  unseal_key               = var.unseal_key
+  cluster_nodes_ids        = var.cluster_nodes_ids
+  cluster_nodes            = var.cluster_nodes
+  cluster_nodes_public_ips = var.cluster_nodes_public_ips
+  ssh_private_key          = var.ssh_private_key
+  ssh_user                 = var.ssh_user
+  ssh_timeout              = var.ssh_timeout
+  gcp_project_id           = var.gcp_project_id
+  gcp_node_role            = var.gcp_node_role
+  gcp_service_account      = var.gcp_service_account
+}
+
+module "vault_cluster_agents" {
+  source              = "git::ssh://git@github.com/bitrockteam/hashicorp-vault-baseline//modules/agent?ref=master"
+  vault_endpoint      = var.vault_endpoint
+  tcp_listener_tls    = var.tcp_listener_tls
+  gcp_project_id      = var.gcp_project_id
+  gcp_auto_auth       = var.gcp_auto_auth
+  gcp_node_role       = var.gcp_node_role
+  gcp_service_account = var.gcp_service_account
+  nodes_ids           = var.nodes_ids
+  nodes               = var.nodes
+  nodes_public_ips    = var.nodes_public_ips
+  ssh_private_key     = var.ssh_private_key
+  ssh_user            = var.ssh_user
+  ssh_timeout         = var.ssh_timeout
+}
+
+module "consul-cluster" {
+  source                   = "git::ssh://git@github.com/bitrockteam/hashicorp-consul-baseline//modules/consul-cluster?ref=master"
+  ssh_private_key          = var.ssh_private_key
+  cluster_nodes_ids        = var.cluster_nodes_ids
+  cluster_nodes            = var.cluster_nodes
+  cluster_nodes_public_ips = var.cluster_nodes_public_ips
+  vault_address            = var.vault_address
+  dc_name                  = var.dc_name
+}
+
+module "nomad-cluster" {
+  source                   = "git::ssh://git@github.com/bitrockteam/hashicorp-nomad-baseline//modules/nomad-cluster?ref=master"
+  ssh_private_key          = var.ssh_private_key
+  cluster_nodes_ids        = var.cluster_nodes_ids
+  cluster_nodes            = var.cluster_nodes
+  cluster_nodes_public_ips = var.cluster_nodes_public_ips
+  dc_name                  = var.dc_name
+}
---
+++ variables.tf
@@ -0,0 +1,66 @@
+variable "gcp_project_id" {
+  type    = string
+  default = null
+}
+variable "gcp_node_role" {
+  type    = string
+  default = null
+}
+variable "gcp_service_account" {
+  type    = string
+  default = null
+}
+variable "gcp_auto_auth" {
+  type    = bool
+  default = false
+}
+variable "unseal_project_id" {
+  type    = string
+  default = null
+}
+variable "unseal_keyring" {
+  type    = string
+  default = null
+}
+variable "unseal_key" {
+  type    = string
+  default = null
+}
+variable "cluster_nodes_ids" {
+  type = list(string)
+}
+variable "cluster_nodes" {
+  type = map(any)
+}
+variable "cluster_nodes_public_ips" {
+  type    = map(any)
+  default = null
+}
+variable "nodes" {
+  type = list(string)
+}
+variable "nodes_ids" {
+  type = map(any)
+}
+variable "nodes_public_ips" {
+  type = map(any)
+}
+variable "ssh_private_key" {
+  type = string
+}
+variable "ssh_user" {
+  type = string
+}
+variable "ssh_timeout" {
+  type = string
+}
+variable "vault_endpoint" {
+  type = string
+}
+variable "tcp_listener_tls" {
+  type    = bool
+  default = false
+}
+variable "dc_name" {
+  type = string
+}
