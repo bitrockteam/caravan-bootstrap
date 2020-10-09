e7a71dd26493b2bec6787c795761deab259ef663
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2020-10-09 10:25:58 +0200

    fmt <3

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "vault_address" {
-    value = module.vault_cluster.vault_address
+  value = module.vault_cluster.vault_address
 }
 
 output "consul_address" {
-    value = module.consul-cluster.consul_address
+  value = module.consul-cluster.consul_address
 }
