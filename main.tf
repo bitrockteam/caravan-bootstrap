5c1fe25223b3c1bd57a560190ff92d44aafdb9f2
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-02-02 16:39:07 +0100

    Update bootstrap with new updates

--- main.tf
+++ main.tf
@@ -1,44 +1,64 @@
 module "vault_cluster" {
-  source                   = "git::ssh://git@github.com/bitrockteam/hashicorp-vault-baseline//modules/cluster-raft?ref=master"
-  cluster_nodes_ids        = var.nodes_ids
-  cluster_nodes            = var.nodes
-  cluster_nodes_public_ips = var.nodes_public_ips
-  ssh_private_key          = var.ssh_private_key
-  ssh_user                 = var.ssh_user
-  unseal_type              = var.unseal_type
-  unseal_region            = var.unseal_region
-  ssh_timeout              = var.ssh_timeout
-  unseal_project_id        = var.unseal_project_id
-  unseal_keyring           = var.unseal_keyring
-  unseal_key               = var.unseal_key
-  gcp_project_id           = var.gcp_project_id
-  gcp_node_role            = var.gcp_node_role
-  gcp_service_account      = var.gcp_service_account
-  prefix                   = var.prefix
+  source                         = "git::ssh://git@github.com/bitrockteam/hashicorp-vault-baseline//modules/cluster-raft?ref=master"
+  control_plane_nodes_ids        = var.control_plane_nodes_ids
+  control_plane_nodes            = var.control_plane_nodes
+  control_plane_nodes_public_ips = var.control_plane_nodes_public_ips
+  ssh_private_key                = var.ssh_private_key
+  ssh_user                       = var.ssh_user
+  ssh_timeout                    = var.ssh_timeout
+  prefix                         = var.prefix
+
+  unseal_type = var.unseal_type
+
+  // TRANSIT
+  transit_vault_address = var.transit_vault_address
+  transit_mount_path    = var.transit_mount_path
+  transit_vault_token   = var.transit_vault_token
+  transit_key           = var.transit_key
+
+  // OCI
+  oci_key                 = var.oci_key
+  oci_crypto_endpoint     = var.oci_crypto_endpoint
+  oci_management_endpoint = var.oci_management_endpoint
+
+  // GCP
+  gcp_region     = var.gcp_region
+  gcp_keyring    = var.gcp_keyring
+  gcp_key        = var.gcp_key
+  gcp_project_id = var.gcp_project_id
+
+  // AWS
+  aws_kms_region = var.aws_region
+  aws_kms_key_id = var.aws_kms_key_id
+  aws_access_key = var.aws_access_key
+  aws_secret_key = var.aws_secret_key
+  aws_endpoint   = var.aws_endpoint
 }
 
 module "vault_cluster_agents" {
-  source              = "git::ssh://git@github.com/bitrockteam/hashicorp-vault-baseline//modules/agent?ref=feature/refactoring"
+  source              = "git::ssh://git@github.com/bitrockteam/hashicorp-vault-baseline//modules/agent?ref=master"
   vault_endpoint      = var.vault_endpoint
   tcp_listener_tls    = var.tcp_listener_tls
   gcp_project_id      = var.gcp_project_id
-  gcp_auto_auth       = var.gcp_auto_auth
+  gcp_auto_auth       = var.agent_auto_auth_type == "gcp" ? true : false
   gcp_node_role       = var.gcp_node_role
   gcp_service_account = var.gcp_service_account
-  nodes_ids           = var.nodes_ids
-  nodes               = var.nodes
-  nodes_public_ips    = var.nodes_public_ips
+  nodes_ids           = var.control_plane_nodes_ids
+  nodes               = var.control_plane_nodes
+  nodes_public_ips    = var.control_plane_nodes_public_ips
   ssh_private_key     = var.ssh_private_key
   ssh_user            = var.ssh_user
   ssh_timeout         = var.ssh_timeout
+  aws_auto_auth       = var.agent_auto_auth_type == "aws" ? true : false
+  aws_node_role       = var.aws_node_role
 }
 
 module "consul-cluster" {
   source                   = "git::ssh://git@github.com/bitrockteam/hashicorp-consul-baseline//modules/consul-cluster?ref=master"
   ssh_private_key          = var.ssh_private_key
-  cluster_nodes_ids        = var.nodes_ids
-  cluster_nodes            = var.nodes
-  cluster_nodes_public_ips = var.nodes_public_ips
+  cluster_nodes_ids        = var.control_plane_nodes_ids
+  cluster_nodes            = var.control_plane_nodes
+  cluster_nodes_public_ips = var.control_plane_nodes_public_ips
   vault_address            = module.vault_cluster.vault_address
   dc_name                  = var.dc_name
 }
@@ -50,9 +70,9 @@ module "nomad-cluster" {
   ]
   source                   = "git::ssh://git@github.com/bitrockteam/hashicorp-nomad-baseline//modules/nomad-cluster?ref=master"
   ssh_private_key          = var.ssh_private_key
-  cluster_nodes_ids        = var.nodes_ids
-  cluster_nodes            = var.nodes
-  cluster_nodes_public_ips = var.nodes_public_ips
+  cluster_nodes_ids        = var.control_plane_nodes_ids
+  cluster_nodes            = var.control_plane_nodes
+  cluster_nodes_public_ips = var.control_plane_nodes_public_ips
   dc_name                  = var.dc_name
   control_plane_vault_role = var.control_plane_role_name
 }
--- variables.tf
+++ variables.tf
@@ -1,46 +1,11 @@
-variable "gcp_project_id" {
-  type    = string
-  default = null
-}
-variable "gcp_node_role" {
-  type    = string
-  default = null
-}
-variable "gcp_service_account" {
-  type    = string
-  default = null
-}
-variable "gcp_auto_auth" {
-  type    = bool
-  default = false
-}
-variable "unseal_project_id" {
-  type    = string
-  default = null
-}
-variable "unseal_keyring" {
-  type    = string
-  default = null
-}
-variable "unseal_key" {
-  type    = string
-  default = null
-}
-variable "unseal_region" {
-  type    = string
-  default = null
-}
-variable "unseal_type" {
-  type    = string
-  default = null
-}
-variable "nodes" {
+// Common
+variable "control_plane_nodes" {
   type = map(any)
 }
-variable "nodes_ids" {
+variable "control_plane_nodes_ids" {
   type = list(string)
 }
-variable "nodes_public_ips" {
+variable "control_plane_nodes_public_ips" {
   type = map(any)
 }
 variable "ssh_private_key" {
@@ -52,10 +17,10 @@ variable "ssh_user" {
 variable "ssh_timeout" {
   type = string
 }
-variable "vault_endpoint" {
+variable "prefix" {
   type = string
 }
-variable "control_plane_vault_role" {
+variable "vault_endpoint" {
   type = string
 }
 variable "control_plane_role_name" {
@@ -68,6 +33,94 @@ variable "tcp_listener_tls" {
 variable "dc_name" {
   type = string
 }
-variable "prefix" {
-  type = string
+variable "unseal_type" {
+  type    = string
+  default = null
+}
+variable "agent_auto_auth_type" {
+  type    = string
+  default = null
+}
+
+// Transit
+variable "transit_vault_address" {
+  default = null
+}
+variable "transit_mount_path" {
+  type    = string
+  default = null
+}
+variable "transit_vault_token" {
+  type    = string
+  default = null
+}
+variable "transit_key" {
+  type    = string
+  default = null
+}
+
+// OCI
+variable "oci_key" {
+  type    = string
+  default = null
+}
+variable "oci_crypto_endpoint" {
+  type    = string
+  default = null
+}
+variable "oci_management_endpoint" {
+  type    = string
+  default = null
+}
+
+// GCP
+variable "gcp_region" {
+  type    = string
+  default = null
+}
+variable "gcp_keyring" {
+  type    = string
+  default = null
+}
+variable "gcp_key" {
+  type    = string
+  default = null
+}
+variable "gcp_project_id" {
+  type    = string
+  default = null
+}
+variable "gcp_node_role" {
+  type    = string
+  default = null
+}
+variable "gcp_service_account" {
+  type    = string
+  default = null
+}
+
+// AWS
+variable "aws_region" {
+  type    = string
+  default = null
+}
+variable "aws_kms_key_id" {
+  type    = string
+  default = null
+}
+variable "aws_node_role" {
+  type    = string
+  default = null
+}
+variable "aws_access_key" {
+  type    = string
+  default = null
+}
+variable "aws_secret_key" {
+  type    = string
+  default = null
+}
+variable "aws_endpoint" {
+  type    = string
+  default = null
 }
