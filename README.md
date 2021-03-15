f65a92c6fff1317bbcae6153ea10d5de48fc19e0
Simone Ripamonti <simone.ripamonti@bitrock.it>
2021-03-15 15:50:26 +0100

    Prepare release (#7)

--- README.md
+++ README.md
@@ -13,10 +13,10 @@ No provider.
 
 | Name | Source | Version |
 |------|--------|---------|
-| consul-cluster | git::ssh://git@github.com/bitrockteam/caravan-consul//modules/consul-cluster?ref=main |  |
-| nomad-cluster | git::ssh://git@github.com/bitrockteam/caravan-nomad//modules/nomad-cluster?ref=main |  |
-| vault_cluster | git::ssh://git@github.com/bitrockteam/caravan-vault//modules/cluster-raft?ref=main |  |
-| vault_cluster_agents | git::ssh://git@github.com/bitrockteam/caravan-vault//modules/agent?ref=main |  |
+| consul-cluster | git::ssh://git@github.com/bitrockteam/caravan-consul//modules/consul-cluster?ref=refs/tags/v0.1.0 |  |
+| nomad-cluster | git::ssh://git@github.com/bitrockteam/caravan-nomad//modules/nomad-cluster?ref=refs/tags/v0.1.5 |  |
+| vault_cluster | git::ssh://git@github.com/bitrockteam/caravan-vault//modules/cluster-raft?ref=refs/tags/v0.3.0 |  |
+| vault_cluster_agents | git::ssh://git@github.com/bitrockteam/caravan-vault//modules/agent?ref=refs/tags/v0.3.0 |  |
 
 ## Resources
 
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vault_cluster" {
-  source                         = "git::ssh://git@github.com/bitrockteam/caravan-vault//modules/cluster-raft?ref=main"
+  source                         = "git::ssh://git@github.com/bitrockteam/caravan-vault//modules/cluster-raft?ref=refs/tags/v0.3.0"
   control_plane_nodes_ids        = var.control_plane_nodes_ids
   control_plane_nodes            = var.control_plane_nodes
   control_plane_nodes_public_ips = var.control_plane_nodes_public_ips
@@ -43,7 +43,7 @@ module "vault_cluster" {
 }
 
 module "vault_cluster_agents" {
-  source              = "git::ssh://git@github.com/bitrockteam/caravan-vault//modules/agent?ref=main"
+  source              = "git::ssh://git@github.com/bitrockteam/caravan-vault//modules/agent?ref=refs/tags/v0.3.0"
   vault_endpoint      = var.vault_endpoint
   tcp_listener_tls    = var.tcp_listener_tls
   gcp_project_id      = var.gcp_project_id
@@ -64,7 +64,7 @@ module "vault_cluster_agents" {
 }
 
 module "consul-cluster" {
-  source                         = "git::ssh://git@github.com/bitrockteam/caravan-consul//modules/consul-cluster?ref=main"
+  source                         = "git::ssh://git@github.com/bitrockteam/caravan-consul//modules/consul-cluster?ref=refs/tags/v0.1.0"
   ssh_private_key                = var.ssh_private_key
   cluster_nodes_ids              = var.control_plane_nodes_ids
   cluster_nodes                  = var.control_plane_nodes
@@ -80,7 +80,7 @@ module "nomad-cluster" {
     module.vault_cluster,
     module.consul-cluster
   ]
-  source                   = "git::ssh://git@github.com/bitrockteam/caravan-nomad//modules/nomad-cluster?ref=main"
+  source                   = "git::ssh://git@github.com/bitrockteam/caravan-nomad//modules/nomad-cluster?ref=refs/tags/v0.1.5"
   ssh_private_key          = var.ssh_private_key
   cluster_nodes_ids        = var.control_plane_nodes_ids
   cluster_nodes            = var.control_plane_nodes
