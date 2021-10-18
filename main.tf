02abe6276bd0abdc5c81586d36b0ef082c0ad020
efbar <17676301+efbar@users.noreply.github.com>
2021-10-18 16:01:31 +0200

    update tags

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vault_cluster" {
-  source                         = "git::https://github.com/bitrockteam/caravan-vault//modules/cluster-raft?ref=refs/tags/v0.3.17"
+  source                         = "git::https://github.com/bitrockteam/caravan-vault//modules/cluster-raft?ref=refs/tags/v0.3.18"
   control_plane_nodes_ids        = var.control_plane_nodes_ids
   control_plane_nodes            = var.control_plane_nodes
   control_plane_nodes_public_ips = var.control_plane_nodes_public_ips
@@ -47,7 +47,7 @@ module "vault_cluster" {
 }
 
 module "vault_cluster_agents" {
-  source                  = "git::https://github.com/bitrockteam/caravan-vault//modules/agent?ref=feat/pluggable_nomad"
+  source                  = "git::https://github.com/bitrockteam/caravan-vault//modules/agent?ref=refs/tags/v0.3.18"
   vault_endpoint          = var.vault_endpoint
   enable_nomad            = var.enable_nomad
   tcp_listener_tls        = var.tcp_listener_tls
